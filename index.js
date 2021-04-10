ea1b0ca45dcffcd930a79a28a746bfb2132d041e
Tan <decodingskill@gmail.com>
2021-04-10 13:26:39 +1000

    Add get method

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // bind app
 app.use(express.json());
-
+//temp data for testing
 const books = 
 [
     {title:'Java program',id: 1 },
@@ -12,9 +12,23 @@ const books =
     {title:'NodeJs program',id: 3 },
 ];
 
+
+
+/**Add End point for  */
+//********get method*******/
 app.get('/',(req,resp)=>
 {
     resp.send('Welcome to my API')
 });
 
+app.get('/api/book',(req,resp)=>
+{
+    resp.send(books)
+
+})
+/**Post Method */
+
+
+
+//set up port 
 app.listen(5400);
